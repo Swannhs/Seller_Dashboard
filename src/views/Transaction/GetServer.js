26d96c1578541d7bbe792e477d03b38ef048cf64
Swann <princeapurbask@gmail.com>
2021-04-03 02:19:41 +0600

    Getting server list from backend

---
+++ src/views/Transaction/GetServer.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import Cookies from "universal-cookie/lib";
+import RadiusApi from "../../radius-api/RadiusApi";
+import {Form} from "react-bootstrap";
+
+class GetServer extends Component {
+    state = {
+        select: '',
+        servers: []
+    }
+
+    componentDidMount() {
+        const token = new Cookies;
+        RadiusApi.get('/server-realms/servers.json',{
+            params: {
+                token: token.get('Token')
+            }
+        })
+            .then(response => {
+                this.setState({
+                    servers: response.data.servers
+                })
+            })
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        prevState !== this.state ? this.props.onChange(this.state.select) : null
+    }
+
+    onFormSubmit = event => {
+        event.preventDefault();
+        this.props.onFormSubmit(this.state.servers)
+    }
+
+    render() {
+        return (
+            <Form.Group>
+                <div className="w-50 p-3">
+                    <h3 className='text-black-50'>Servers</h3>
+                    <Form.Control
+                        as="select"
+                        className="mr-sm-2"
+                        id="inlineFormCustomSelect"
+                        custom
+                        value={this.state.select}
+                        onChange={event => {
+                            this.setState({
+                                select: event.target.value
+                            })
+                        }}
+                    >
+                        <option>Choose...</option>
+
+                        {this.state.servers.map((items) => {
+                            return (
+                                <>
+                                    <option key={items.id} value={items.id}>{items.name}</option>
+                                </>
+                            )
+                        })}
+                    </Form.Control>
+                </div>
+            </Form.Group>
+        );
+    }
+}
+
+export default GetServer;
