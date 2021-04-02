dc5fbf9ab9c2f487d46246638235669c05233ef6
Swann <princeapurbask@gmail.com>
2021-04-03 02:19:19 +0600

    Declared UI for creating tweak with user friendly UI

---
+++ src/views/tweak/CreateTweak.js
@@ -0,0 +1,122 @@
+import React, {Component} from 'react';
+import {Input} from "reactstrap";
+import RadiusApi from "../../radius-api/RadiusApi";
+import Cookies from "universal-cookie/lib";
+import {Link} from "react-router-dom";
+
+class CreateTweak extends Component {
+    state = {
+        name: '',
+        vendor: '',
+        protocol: '',
+        injectionType: '',
+        payload: '',
+        note: ''
+    }
+
+    onCreateTweak = () => {
+        let data = this.state
+        let cookie = new Cookies
+        RadiusApi.post('/tweaks/add.json', data, {
+            params: {
+                token: cookie.get('Token')
+            }
+        })
+            .then(response => {
+                console.log(response)
+            })
+    }
+
+    render() {
+        return (
+            <div className='container'>
+                <div className='ml-3'>
+                    <Link to='/admin/root/tweak'>
+                        <button className='ui button'>Back</button>
+                    </Link>
+                </div>
+                <article className="card-body mx-auto" style={{maxWidth: '350px', fontSize: '20px'}}>
+                    <h3>Name</h3>
+                    <Input type='text'
+                           placeholder='Define a name'
+                           value={this.state.name}
+                           onChange={event => {
+                               this.setState({
+                                   name: event.target.value
+                               })
+                           }}
+                           required={true}
+                    />
+
+                    <h3>Vendor</h3>
+                    <Input type='text'
+                           placeholder='Enter vendor name'
+                           value={this.state.vendor}
+                           onChange={event => {
+                               this.setState({
+                                   vendor: event.target.value
+                               })
+                           }}
+                           required={true}
+                    />
+
+                    <h3>Protocol</h3>
+                    <Input type='text'
+                           placeholder='Enter the protocol'
+                           value={this.state.protocol}
+                           onChange={event => {
+                               this.setState({
+                                   protocol: event.target.value
+                               })
+                           }}
+                           required={true}
+                    />
+
+                    <h3>Injection Type</h3>
+                    <Input type='text'
+                           placeholder='Injection'
+                           value={this.state.injectionType}
+                           onChange={event => {
+                               this.setState({
+                                   injectionType: event.target.value
+                               })
+                           }}
+                           required={true}
+                    />
+
+                    <h3>Payload</h3>
+                    <Input type='text'
+                           placeholder='Payload'
+                           value={this.state.payload}
+                           onChange={event => {
+                               this.setState({
+                                   payload: event.target.value
+                               })
+                           }}
+                           required={true}
+                    />
+
+                    <h3>Note</h3>
+                    <Input type='text'
+                           placeholder='Note'
+                           value={this.state.note}
+                           onChange={event => {
+                               this.setState({
+                                   note: event.target.value
+                               })
+                           }}
+                           required={true}
+                    />
+
+                    <button className='ui button primary mt-4'
+                            onClick={this.onCreateTweak}
+                    >
+                        Transfer
+                    </button>
+                </article>
+            </div>
+        );
+    }
+}
+
+export default CreateTweak;
