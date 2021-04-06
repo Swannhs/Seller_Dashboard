1127e14af2b94dafd78b3c8c1493b7782cc65625
Swann <princeapurbask@gmail.com>
2021-04-07 04:53:44 +0600

    It's working fine

--- src/views/Transaction/Admin/TransactionAdmin.js
+++ src/views/Transaction/Admin/TransactionAdmin.js
@@ -5,8 +5,27 @@ import TransactionAdminSend from "./TransactionAdminSend";
 
 class TransactionAdmin extends Component {
     state = {
-        send: true
+        send: ''
     }
+
+    componentDidMount() {
+        this.setState({
+            send: false
+        })
+    }
+
+    onTrueHandel = () => {
+        this.setState({
+            sent: false
+        })
+    }
+
+    onFalseHandel = () => {
+        this.setState({
+            sent: true
+        })
+    }
+
 
     render() {
         return (
@@ -29,30 +48,24 @@ class TransactionAdmin extends Component {
                     <div className="ui buttons">
                         <button className="ui button red"
                                 onClick={() => {
-                                    this.setState({
-                                        sent: true
-                                    })
+                                    this.onTrueHandel()
                                 }}
                         >Sent
                         </button>
                         <div className="or"/>
                         <button className="ui button positive"
                                 onClick={() => {
-                                    this.setState({
-                                        sent: false
-                                    })
+                                    this.onFalseHandel()
                                 }}>
                             Received
                         </button>
                     </div>
                 </div>
-
-                {this.state.sent ? <TransactionAdminSend id={this.props.match.params.id}/> :
-                    <TransactionAdminReceived id={this.props.match.params.id}/>}
+                {this.state.sent ? <TransactionAdminReceived id={this.props.match.params.id}/> :
+                    <TransactionAdminSend id={this.props.match.params.id}/>}
             </>
         );
     }
-
 }
 
 export default TransactionAdmin;
