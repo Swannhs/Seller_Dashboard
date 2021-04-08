0e35efe524c9bc5b7c44e82f0c15e7cddeb38a95
Swann <princeapurbask@gmail.com>
2021-04-08 14:45:48 +0600

    Fixed all transaction issue

--- src/views/Transaction/Admin/TransactionAdminSend.js
+++ src/views/Transaction/Admin/TransactionAdminSend.js
@@ -48,41 +48,50 @@ class TransactionAdminSend extends Component {
     render() {
         return (
             <>
-                <table className="table table-striped">
-                    <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Trx ID</th>
-                        <th scope="col">Partner</th>
-                        <th scope="col">Profile</th>
-                        <th scope="col">Group</th>
-                        <th scope="col">credit</th>
-                        <th scope="col">Debit</th>
-                        <th scope="col">Cost</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        this.state.transactions.length ? this.state.transactions.map((item) => {
-                            return (
-                                <tr key={item.id}>
-                                    <td>{item.id}</td>
-                                    <td>{item.transaction}</td>
-                                    <td>{item.user.username}</td>
-                                    <td>{item.profile.name}</td>
-                                    <td>{item.realm.name}</td>
-                                    <td>{item.credit}</td>
-                                    <td>{item.debit}</td>
-                                    <td>{item.balance}</td>
-                                </tr>
-                            )
-                        }) : <h3 className='text-center text-danger'>No send history yet</h3>
-                    }
-                    </tbody>
-                </table>
+                {this.state.root ?
+                    <>
+                        {
+                            this.state.transactions.length ?
+                                <table className="table table-striped">
+                                    <thead>
+                                    <tr>
+                                        <th scope="col">ID</th>
+                                        <th scope="col">Trx ID</th>
+                                        <th scope="col">Partner</th>
+                                        <th scope="col">Profile</th>
+                                        <th scope="col">Group</th>
+                                        <th scope="col">credit</th>
+                                        <th scope="col">Debit</th>
+                                        <th scope="col">Cost</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    {
+                                        this.state.transactions.map((item) => {
+                                            return (
+                                                <tr key={item.id}>
+                                                    <td>{item.id}</td>
+                                                    <td>{item.transaction}</td>
+                                                    <td>{item.user.username}</td>
+                                                    <td>{item.profile.name}</td>
+                                                    <td>{item.realm.name}</td>
+                                                    <td>{item.credit}</td>
+                                                    <td>{item.debit}</td>
+                                                    <td>{item.balance}</td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
+                                    </tbody>
+                                </table> : <h3 className='text-center text-danger'>No send history yet</h3>
+                        }
+                    </>
+                    : <h3 className='text-center text-danger'>You are not allowed here</h3>
+                }
             </>
         );
     }
+
 }
 
 export default TransactionAdminSend;
--- src/views/Transaction/AgentTransaction/TransactionReceive.js
+++ src/views/Transaction/AgentTransaction/TransactionReceive.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import Cookies from "universal-cookie/lib";
 import RadiusApi from "../../../radius-api/RadiusApi";
-import {Link} from "react-router-dom";
 
 class TransactionReceive extends Component {
     state = {
@@ -28,41 +27,45 @@ class TransactionReceive extends Component {
 
     render() {
         return (
-            <table className="table table-striped">
-                <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Trx ID</th>
-                    <th scope="col">Partner</th>
-                    <th scope="col">Profile</th>
-                    <th scope="col">Group</th>
-                    <th scope="col">credit</th>
-                    <th scope="col">Debit</th>
-                    <th scope="col">Balance</th>
-                </tr>
-                </thead>
-                <tbody>
-                {
-                    this.state.transactions ? this.state.transactions.map((item) => {
-                        return (
-                            <tr key={item.id}>
-                                <td>{item.id}</td>
-                                <td>{item.transaction}</td>
-                                <td>{item.user.username}</td>
-                                <td>{item.profile.name}</td>
-                                <td>{item.realm.name}</td>
-                                <td>{item.credit}</td>
-                                <td>{item.debit}</td>
-                                <td>{item.balance}</td>
-                            </tr>
-                        )
-                    }) : <h3 className='text-center text-danger'>No Received history yet</h3>
+            <>
+                {this.state.transactions.length ?
+                    <table className="table table-striped">
+                        <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Trx ID</th>
+                            <th scope="col">Partner</th>
+                            <th scope="col">Profile</th>
+                            <th scope="col">Group</th>
+                            <th scope="col">credit</th>
+                            <th scope="col">Debit</th>
+                            <th scope="col">Balance</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            this.state.transactions.map((item) => {
+                                return (
+                                    <tr key={item.id}>
+                                        <td>{item.id}</td>
+                                        <td>{item.transaction}</td>
+                                        <td>{item.user.username}</td>
+                                        <td>{item.profile.name}</td>
+                                        <td>{item.realm.name}</td>
+                                        <td>{item.credit}</td>
+                                        <td>{item.debit}</td>
+                                        <td>{item.balance}</td>
+                                    </tr>
+                                )
+                            })
+                        }
+                        </tbody>
+                    </table>
+                    : <h3 className='text-center text-danger'>No Received history yet</h3>
                 }
-                </tbody>
-            </table>
+            </>
         );
     }
-
 }
 
 export default TransactionReceive;
