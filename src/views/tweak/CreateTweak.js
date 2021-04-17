f0085ac2932f4d9830ca51bfaf71433d536e8360
Swann <princeapurbask@gmail.com>
2021-04-18 02:29:57 +0600

    Validation added in createTweak

--- src/views/tweak/CreateTweak.js
+++ src/views/tweak/CreateTweak.js
@@ -36,83 +36,84 @@ class CreateTweak extends Component {
                     </Link>
                 </div>
                 <article className="card-body mx-auto" style={{maxWidth: '350px', fontSize: '20px'}}>
-                    <h3>Name</h3>
-                    <Input type='text'
-                           placeholder='Define a name'
-                           value={this.state.name}
-                           onChange={event => {
-                               this.setState({
-                                   name: event.target.value
-                               })
-                           }}
-                           required={true}
-                    />
+                    <form action={this.onCreateTweak}>
+                        <h3>Name</h3>
+                        <Input type='text'
+                               placeholder='Define a name'
+                               value={this.state.name}
+                               onChange={event => {
+                                   this.setState({
+                                       name: event.target.value
+                                   })
+                               }}
+                               required={true}
+                        />
 
-                    <h3>Vendor</h3>
-                    <Input type='text'
-                           placeholder='Enter vendor name'
-                           value={this.state.vendor}
-                           onChange={event => {
-                               this.setState({
-                                   vendor: event.target.value
-                               })
-                           }}
-                           required={true}
-                    />
+                        <h3>Vendor</h3>
+                        <Input type='text'
+                               placeholder='Enter vendor name'
+                               value={this.state.vendor}
+                               onChange={event => {
+                                   this.setState({
+                                       vendor: event.target.value
+                                   })
+                               }}
+                               required={true}
+                        />
 
-                    <h3>Protocol</h3>
-                    <Input type='text'
-                           placeholder='Enter the protocol'
-                           value={this.state.protocol}
-                           onChange={event => {
-                               this.setState({
-                                   protocol: event.target.value
-                               })
-                           }}
-                           required={true}
-                    />
+                        <h3>Protocol</h3>
+                        <Input type='text'
+                               placeholder='Enter the protocol'
+                               value={this.state.protocol}
+                               onChange={event => {
+                                   this.setState({
+                                       protocol: event.target.value
+                                   })
+                               }}
+                               required={true}
+                        />
 
-                    <h3>Injection Type</h3>
-                    <Input type='text'
-                           placeholder='Injection'
-                           value={this.state.injectionType}
-                           onChange={event => {
-                               this.setState({
-                                   injectionType: event.target.value
-                               })
-                           }}
-                           required={true}
-                    />
+                        <h3>Injection Type</h3>
+                        <Input type='text'
+                               placeholder='Injection'
+                               value={this.state.injectionType}
+                               onChange={event => {
+                                   this.setState({
+                                       injectionType: event.target.value
+                                   })
+                               }}
+                               required={true}
+                        />
 
-                    <h3>Payload</h3>
-                    <Input type='text'
-                           placeholder='Payload'
-                           value={this.state.payload}
-                           onChange={event => {
-                               this.setState({
-                                   payload: event.target.value
-                               })
-                           }}
-                           required={true}
-                    />
+                        <h3>Payload</h3>
+                        <Input type='text'
+                               placeholder='Payload'
+                               value={this.state.payload}
+                               onChange={event => {
+                                   this.setState({
+                                       payload: event.target.value
+                                   })
+                               }}
+                               required={true}
+                        />
 
-                    <h3>Note</h3>
-                    <Input type='text'
-                           placeholder='Note'
-                           value={this.state.note}
-                           onChange={event => {
-                               this.setState({
-                                   note: event.target.value
-                               })
-                           }}
-                           required={true}
-                    />
+                        <h3>Note</h3>
+                        <Input type='text'
+                               placeholder='Note'
+                               value={this.state.note}
+                               onChange={event => {
+                                   this.setState({
+                                       note: event.target.value
+                                   })
+                               }}
+                               required={true}
+                        />
 
-                    <button className='ui button primary mt-4'
-                            onClick={this.onCreateTweak}
-                    >
-                        Transfer
-                    </button>
+                        <button type='submit' className='ui button primary mt-4'>
+                            Create
+                        </button>
+                    </form>
+
                 </article>
             </div>
         );
