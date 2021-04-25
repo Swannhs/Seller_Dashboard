22ab403bde65346d37fb5e01417acd44e7b8b678
Swann <princeapurbask@gmail.com>
2021-04-25 22:10:53 +0600

    Configured voucher transaction sent in a dynamic view

--- src/views/Transaction/AgentTransaction/TransactionSent.js
+++ src/views/Transaction/AgentTransaction/TransactionSent.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
 import Cookies from "universal-cookie/lib";
 import RadiusApi from "../../../radius-api/RadiusApi";
+import TransactionSentMobile from "./TransactionSentMobile";
 
 class TransactionSent extends Component {
     state = {
-        transactions: []
+        transactions: [],
+        mobile: false
     }
 
     componentDidMount() {
+        this.setState({mobile: window.innerWidth <= 660})
+
         const cookie = new Cookies();
 
         RadiusApi.get('/voucher-transactions/view.json', {
@@ -28,44 +32,49 @@ class TransactionSent extends Component {
     render() {
         return (
             <>
-                {this.state.transactions.length ?
-                    <table className="table table-striped">
-                        <thead>
-                        <tr>
-                            <th scope="col">ID</th>
-                            <th scope="col">Trx ID</th>
-                            <th scope="col">Partner</th>
-                            <th scope="col">Profile</th>
-                            <th scope="col">Group</th>
-                            <th scope="col">credit</th>
-                            <th scope="col">Debit</th>
-                            <th scope="col">Balance</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {
-                            this.state.transactions.map((item) => {
-                                return (
-                                    <tr key={item.id}>
-                                        <td>{item.id}</td>
-                                        <td>{item.transaction}</td>
-                                        <td>{item.user.username}</td>
-                                        <td>{item.profile.name}</td>
-                                        <td>{item.realm.name}</td>
-                                        <td>{item.credit}</td>
-                                        <td>{item.debit}</td>
-                                        <td>{item.balance}</td>
-                                    </tr>
-                                )
-                            })
+                {this.state.mobile ? <TransactionSentMobile data={this.state.transactions}/> :
+                    <>
+                        {this.state.transactions.length ?
+                            <table className="table table-striped">
+                                <thead>
+                                <tr>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">Trx ID</th>
+                                    <th scope="col">Partner</th>
+                                    <th scope="col">Profile</th>
+                                    <th scope="col">Group</th>
+                                    <th scope="col">credit</th>
+                                    <th scope="col">Debit</th>
+                                    <th scope="col">Cost</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {
+                                    this.state.transactions.map((item) => {
+                                        return (
+                                            <tr key={item.id}>
+                                                <td>{item.id}</td>
+                                                <td>{item.transaction}</td>
+                                                <td>{item.user.username}</td>
+                                                <td>{item.profile.name}</td>
+                                                <td>{item.realm.name}</td>
+                                                <td>{item.credit}</td>
+                                                <td>{item.debit}</td>
+                                                <td>{item.balance}$</td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                                </tbody>
+                            </table>
+                            : <h3 className='text-center text-danger'>No send history yet</h3>
                         }
-                        </tbody>
-                    </table>
-                    : <h3 className='text-center text-danger'>No send history yet</h3>
+                    </>
                 }
-
             </>
         );
     }
+
 }
+
 export default TransactionSent;
