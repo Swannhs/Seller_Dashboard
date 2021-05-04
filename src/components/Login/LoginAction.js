07ec50176289a997cc6fd9e7ef2f01ef978090ba
Swannhs <princeapurbask@gmail.com>
2021-05-04 21:27:33 +0600

    Fix login issue having token

--- src/components/Login/LoginAction.js
+++ src/components/Login/LoginAction.js
@@ -3,52 +3,49 @@ import Cookies from "universal-cookie";
 import RadiusApi from "../../radius-api/RadiusApi";
 
 class LoginAction extends Component {
-
-    // componentDidMount() {
-    //     const cookie = new Cookies;
-    //
-    //     if (cookie.get('Token') && cookie.get('Role')){
-    //         RadiusApi.get('/dashboard/checkToken.json',{
-    //             params: {
-    //                 token: cookie.get('Token')
-    //             }
-    //         })
-    //             .then(response => {
-    //
-    //                 if (response.data.success){
-    //                     this.props.history.push(
-    //                         window.location.pathname !== '/' ?
-    //                             this.props.history.push(window.location.pathname) : this.props.history.push('/admin/dashboard')
-    //                     )
-    //                 }
-    //                 else {
-    //                     this.props.history.push('/login');
-    //                 }
-    //             })
-    //     }
-    //     else {
-    //         this.props.history.push('/login');
-    //     }
-    // }
+    state = {
+        loading: true
+    }
 
 
     componentDidMount() {
         const cookie = new Cookies;
 
         if (cookie.get('Token') && cookie.get('Role')) {
-            this.props.history.push(
-                window.location.pathname !== '/' ?
-                    this.props.history.push(window.location.pathname) : this.props.history.push('/admin/dashboard')
-            )
+            RadiusApi.get('/dashboard/check-token.json', {
+                params: {
+                    token: cookie.get('Token')
+                }
+            })
+                .then(response => {
+                    if (response.data.success) {
+                        this.setState({
+                            loading: false
+                        })
+                        this.props.history.push(
+                            window.location.pathname !== '/' ?
+                                this.props.history.push(window.location.pathname) : this.props.history.push('/admin/dashboard')
+                        )
+                    } else {
+                        cookie.remove('Token')
+                        cookie.remove('Role')
+                        this.props.history.push('/login');
+                    }
+                })
 
         } else {
+            cookie.remove('Token')
+            cookie.remove('Role')
             this.props.history.push('/login');
         }
     }
 
     render() {
-        return null
+        return <>
+            {this.state.loading ? <div className="mt-5 ui active centered inline loader mt-3"/> : <></>}
+        </>
     }
+
 }
 
 export default LoginAction;
