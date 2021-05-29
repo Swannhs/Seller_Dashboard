486aa9c1c2a27a5b28f8e1e85783eb121ae0d57c
Swannhs <princeapurbask@gmail.com>
2021-05-30 03:48:28 +0600

    Implemented pagination in credit send for agent

--- src/views/Transaction/AgentTransaction/TransactionSent.js
+++ src/views/Transaction/AgentTransaction/TransactionSent.js
@@ -3,6 +3,7 @@ import Cookies from "universal-cookie/lib";
 import RadiusApi from "../../../radius-api/RadiusApi";
 import TransactionSentMobile from "./TransactionSentMobile";
 import {Pagination} from "semantic-ui-react";
+import {isMobile} from "react-device-detect";
 
 class TransactionSent extends Component {
     state = {
@@ -24,7 +25,10 @@ class TransactionSent extends Component {
         RadiusApi.get('/voucher-transactions/view.json', {
             params: {
                 token: cookie.get('Token'),
-                key: this.props.id
+                key: this.props.id,
+                page: this.state.page,
+                start: this.state.start,
+                limit: this.state.limit,
             }
         })
             .then(response => {
@@ -71,67 +75,70 @@ class TransactionSent extends Component {
     render() {
         return (
             <>
-                {this.state.loading ? <div className="ui active centered inline loader mt-5"/> :
-                    <>
-                        {this.state.mobile ? <TransactionSentMobile data={this.state.transactions}/> :
-                            <>
-                                {this.state.transactions.length ?
-                                    <table className="table table-striped">
-                                        <thead>
-                                        <tr>
-                                            <th scope="col">Trx ID</th>
-                                            <th scope="col">Partner</th>
-                                            <th scope="col">Profile</th>
-                                            <th scope="col">Group</th>
-                                            <th scope="col">credit</th>
-                                            <th scope="col">Debit</th>
-                                            <th scope="col">Cost</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
+                {
+                    this.state.loading ? <div className="ui active centered inline loader mt-5"/> :
+                        <>
+                            {
+                                isMobile ? <TransactionSentMobile data={this.state.transactions}/> :
+                                    <>
                                         {
-                                            this.state.transactions.map((item) => {
-                                                return (
-                                                    <tr key={item.id}>
-                                                        <td>{item.transaction}</td>
-                                                        <td>{item.user.username}</td>
-                                                        <td>{item.profile.name}</td>
-                                                        <td>{item.realm.name}</td>
-                                                        <td>{item.credit}</td>
-                                                        <td>{item.debit}</td>
-                                                        <td>{item.balance}$</td>
+                                            this.state.transactions.length ?
+                                                <table className="table table-striped">
+                                                    <thead>
+                                                    <tr>
+                                                        <th scope="col">Trx ID</th>
+                                                        <th scope="col">Partner</th>
+                                                        <th scope="col">Profile</th>
+                                                        <th scope="col">Group</th>
+                                                        <th scope="col">credit</th>
+                                                        <th scope="col">Debit</th>
+                                                        <th scope="col">Cost</th>
                                                     </tr>
-                                                )
-                                            })
+                                                    </thead>
+                                                    <tbody>
+                                                    {
+                                                        this.state.transactions.map((item) => {
+                                                            return (
+                                                                <tr key={item.id}>
+                                                                    <td>{item.transaction}</td>
+                                                                    <td>{item.user.username}</td>
+                                                                    <td>{item.profile.name}</td>
+                                                                    <td>{item.realm.name}</td>
+                                                                    <td>{item.credit}</td>
+                                                                    <td>{item.debit}</td>
+                                                                    <td>{item.balance}$</td>
+                                                                </tr>
+                                                            )
+                                                        })
+                                                    }
+                                                    </tbody>
+                                                </table>
+                                                : <h3 className='text-center text-danger'>No send history yet</h3>
                                         }
-                                        </tbody>
-                                    </table>
-                                    : <h3 className='text-center text-danger'>No send history yet</h3>
-                                }
-                                {/*--------------------Pagination------------------------*/}
-                                <tfoot>
-                                <tr>
-                                    <th colSpan={5}>
-                                        <div className="ui right floated pagination menu align-content-lg-end">
-                                            <Pagination
-                                                defaultActivePage={this.state.page}
-                                                firstItem={null}
-                                                lastItem={null}
-                                                pointing
-                                                secondary
-                                                totalPages={this.onPagination()}
-                                                onPageChange={async (event, data) =>
-                                                    this.onPageChaneHandler(event, data)
-                                                }
-                                            />
-                                        </div>
-                                    </th>
-                                </tr>
-                                </tfoot>
-                            </>
-                        }
-                    </>
+                                    </>
+                            }
+                        </>
                 }
+                {/*--------------------Pagination------------------------*/}
+                <tfoot>
+                <tr>
+                    <th colSpan={5}>
+                        <div className="ui right floated pagination menu align-content-lg-end">
+                            <Pagination
+                                defaultActivePage={this.state.page}
+                                firstItem={null}
+                                lastItem={null}
+                                pointing
+                                secondary
+                                totalPages={this.onPagination()}
+                                onPageChange={async (event, data) =>
+                                    this.onPageChaneHandler(event, data)
+                                }
+                            />
+                        </div>
+                    </th>
+                </tr>
+                </tfoot>
             </>
         );
     }
