26728919ae3ab8d63e8eebaae02378cdd2db63eb
Swannhs <princeapurbask@gmail.com>
2021-06-06 17:04:27 +0600

    Renamed and moved to Dropdown directory

--- src/views/Transaction/Transfer.js
+++ src/views/Transaction/Transfer.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Link} from "react-router-dom";
-import AllUser from "./AllUser";
+import AllUser from "../../components/Dropdown/AllUser";
 import Cookies from "universal-cookie/lib";
 import RadiusApi from "../../radius-api/RadiusApi";
 import VoucherGroup from "../Voucher/CreateVoucher/VoucherGroup";
