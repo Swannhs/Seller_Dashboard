2a97f01d7d5b4fd33b41fc3fa1fc56529221b5df
Swannhs <princeapurbask@gmail.com>
2021-06-07 07:51:15 +0600

    Removed Payments & Servers

--- public/config.js
+++ public/config.js
@@ -1,6 +1,5 @@
 window.env = {
-    // REACT_APP_BASE_URL: 'http://192.168.0.110:8180/cake3/rd_cake',
-    REACT_APP_BASE_URL: 'http://localhost/cake3/rd_cake',
+    REACT_APP_BASE_URL: 'http://192.168.0.110:8180/cake3/rd_cake',
     APP_NAME: 'World Network'
 }
 
