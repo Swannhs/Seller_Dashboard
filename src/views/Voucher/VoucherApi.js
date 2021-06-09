e2967b6c04955edf88bc853bcc35639ae89fcc61
Swannhs <princeapurbask@gmail.com>
2021-06-09 23:47:01 +0600

    Just formated

--- src/views/Voucher/VoucherApi.js
+++ src/views/Voucher/VoucherApi.js
@@ -51,52 +51,63 @@ class VoucherApi extends Component {
     render() {
         return (
             <>
-                <thead>
-                <tr className='ct-grid-background border-primary'>
+                {
+                    this.props.data.length ?
+                        <>
+                            <thead>
+                            <tr className='ct-grid-background border-primary'>
 
-                    <th scope="col">Name</th>
-                    <th scope="col">Password</th>
-                    <th scope="col">Batch</th>
-                    <th scope="col">Vendor</th>
-                    <th scope="col">Plan</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">Validity</th>
-                    <th scope="col">Action</th>
-                </tr>
-                </thead>
-                <tbody>
-                {(this.props.data) ? this.props.data.map((item) => {
-                    return (
-                        <tr key={item.id}>
+                                <th scope="col">Name</th>
+                                <th scope="col">Password</th>
+                                <th scope="col">Batch</th>
+                                <th scope="col">Vendor</th>
+                                <th scope="col">Plan</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Validity</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {
+                                this.props.data.map((item) => {
+                                    return (
+                                        <tr key={item.id}>
 
-                            {/*{this.props.mobile?<td>*/}
-                            {/*    <AiOutlinePlusCircle/>*/}
-                            {/*</td>:null}*/}
+                                            {/*{this.props.mobile?<td>*/}
+                                            {/*    <AiOutlinePlusCircle/>*/}
+                                            {/*</td>:null}*/}
 
-                            <td>{item.name}</td>
-                            <td>{item.password}</td>
-                            <td>{item.batch}</td>
-                            <td>{item.realm}</td>
-                            <td>{item.profile}</td>
-                            <td>
-                                {
-                                    item.status === 'new'? <span className="ui green label small">New</span>:
-                                        item.status === 'used' ? <span className="ui yellow label small">Used</span> :
-                                            <span className="ui red label small text-capitalize">{item.status}</span>
-                                }
-                            </td>
-                            <td>{item.time_valid_in_words}</td>
-                            {/*<td>{item.active ? <span>Active</span> : <span>Inactive</span>}</td>*/}
-                            <td data-label="Action">
-                                <Button className='btn-sm btn-danger' onClick={() => this.onVoucherConfirm(item.id)}>
-                                    <BiReset aria-placeholder='reset'/>
-                                </Button>
-                            </td>
-                        </tr>
-                    )
-                }) : null
+                                            <td>{item.name}</td>
+                                            <td>{item.password}</td>
+                                            <td>{item.batch}</td>
+                                            <td>{item.realm}</td>
+                                            <td>{item.profile}</td>
+                                            <td>
+                                                {
+                                                    item.status === 'new' ?
+                                                        <span className="ui green label small">New</span> :
+                                                        item.status === 'used' ?
+                                                            <span className="ui yellow label small">Used</span> :
+                                                            <span
+                                                                className="ui red label small text-capitalize">{item.status}</span>
+                                                }
+                                            </td>
+                                            <td>{item.time_valid_in_words}</td>
+                                            {/*<td>{item.active ? <span>Active</span> : <span>Inactive</span>}</td>*/}
+                                            <td data-label="Action">
+                                                <Button className='btn-sm btn-danger'
+                                                        onClick={() => this.onVoucherConfirm(item.id)}>
+                                                    <BiReset aria-placeholder='reset'/>
+                                                </Button>
+                                            </td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                            </tbody>
+                        </>
+                        : <h3 className='text-center text-danger'>No voucher found</h3>
                 }
-                </tbody>
             </>
         );
     }
