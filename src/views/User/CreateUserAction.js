c1f4e345940f8e04c06ee781ff978e6383fec020
Swannhs <princeapurbask@gmail.com>
2021-06-10 14:27:44 +0600

    Resolve cookie

--- src/views/User/CreateUserAction.js
+++ src/views/User/CreateUserAction.js
@@ -1,18 +1,15 @@
 import React, {Component} from 'react';
 import './CreateUser.css';
 import RadiusApi from "../../radius-api/RadiusApi";
-import Cookies from "universal-cookie/lib";
 import CreateUserUi from "./CreateUserUI";
 import {confirmAlert} from "react-confirm-alert";
 
 class CreateUserAction extends Component {
 
     onCreateUser = data => {
-        const cookie = new Cookies();
-
         RadiusApi.post('/access-providers/add.json', data, {
             params: {
-                token: cookie.get('Token')
+                token: localStorage.getItem('Token')
             }
         })
             .then(response => {
