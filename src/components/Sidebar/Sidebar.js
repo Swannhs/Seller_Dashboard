1b96f31cf7ece23ee411bd62d53e6c5ee51c08a2
Swannhs <princeapurbask@gmail.com>
2021-06-10 14:33:24 +0600

    Customized sidebar based on user demand

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -31,7 +31,7 @@ class Sidebar extends Component {
     //----------------Sidebar open button-------------------------
     mobileSidebarToggle = (e) => {
         e.preventDefault();
-        if (isMobile){
+        if (isMobile) {
             document.getElementsByClassName('nav-open')[0].className = '';
         }
 
@@ -43,124 +43,124 @@ class Sidebar extends Component {
 
         return (
             <>
-                {/*{*/}
-                {/*    isMobile ? <></> :*/}
-                <div className="sidebar">
-                    <div className="sidebar-wrapper">
-                        <div className="logo d-flex align-items-center justify-content-start">
-                            <Link
-                                to="/admin/dashboard"
-                                className="simple-text logo-mini mx-1"
-                            >
-                            </Link>
-                            <Link className="simple-text text-capitalize" to='/admin/dashboard'>
-                                {this.state.app_name}
-                            </Link>
-                        </div>
-                        <Nav>
+                {
+                    isMobile ? <></> :
+                        <div className="sidebar">
+                            <div className="sidebar-wrapper">
+                                <div className="logo d-flex align-items-center justify-content-start">
+                                    <Link
+                                        to="/admin/dashboard"
+                                        className="simple-text logo-mini mx-1"
+                                    >
+                                    </Link>
+                                    <Link className="simple-text text-capitalize" to='/admin/dashboard'>
+                                        {this.state.app_name}
+                                    </Link>
+                                </div>
+                                <Nav>
 
-                            <li>
-                                <ProSidebar>
-                                    <Menu iconShape="square">
-                                        <MenuItem icon={<AiFillDashboard/>}
-                                                  onClick={() => this.mobileSidebarToggle(event)}
-                                        >
-                                            <Link to='/admin/dashboard'>
-                                                Dashboard
-                                            </Link>
-                                        </MenuItem>
-                                        {
-                                            this.state.role === 'admin' || this.state.role === 'agent' ?
-
-                                                <MenuItem icon={<AiOutlineUser/>}
-                                                    onClick={() => this.mobileSidebarToggle(event)}
+                                    <li>
+                                        <ProSidebar>
+                                            <Menu iconShape="square">
+                                                <MenuItem icon={<AiFillDashboard/>}
+                                                          onClick={() => this.mobileSidebarToggle(event)}
                                                 >
-                                                    <Link to='/admin/users/view'>
-                                                        Sellers
+                                                    <Link to='/admin/dashboard'>
+                                                        Dashboard
+                                                    </Link>
+                                                </MenuItem>
+                                                {
+                                                    this.state.role === 'admin' || this.state.role === 'agent' ?
+
+                                                        <MenuItem icon={<AiOutlineUser/>}
+                                                                  onClick={() => this.mobileSidebarToggle(event)}
+                                                        >
+                                                            <Link to='/admin/users/view'>
+                                                                Resellers
+                                                            </Link>
+                                                        </MenuItem>
+
+                                                        : <></>
+                                                }
+
+                                                <MenuItem icon={<BsListNested/>}
+                                                          onClick={() => this.mobileSidebarToggle(event)}
+                                                >
+                                                    <Link to='/admin/voucher/view'>
+                                                        Vouchers
                                                     </Link>
                                                 </MenuItem>
 
-                                                : <></>
-                                        }
+                                                <MenuItem icon={<BiMoney/>}
+                                                          onClick={() => this.mobileSidebarToggle(event)}
+                                                >
 
-                                        <MenuItem icon={<BsListNested/>}
-                                                  onClick={() => this.mobileSidebarToggle(event)}
-                                        >
-                                            <Link to='/admin/voucher/view'>
-                                                Vouchers
-                                            </Link>
-                                        </MenuItem>
+                                                    {
+                                                        this.state.role === 'admin' ?
+                                                            <Link to='/admin/root/voucher/transaction'>
+                                                                Credits
+                                                            </Link>
+                                                            : <Link to='/admin/voucher/transaction'>
+                                                                Credits
+                                                            </Link>
+                                                    }
 
-                                        <MenuItem icon={<BiMoney/>}
-                                                  onClick={() => this.mobileSidebarToggle(event)}
-                                        >
+                                                </MenuItem>
 
-                                            {
-                                                this.state.role === 'admin' ?
-                                                    <Link to='/admin/root/voucher/transaction'>
-                                                        Credits
-                                                    </Link>
-                                                    : <Link to='/admin/voucher/transaction'>
-                                                        Credits
-                                                    </Link>
-                                            }
+                                                {/*<MenuItem icon={<MdAttachMoney/>}>*/}
+                                                {/*    {*/}
+                                                {/*        this.state.role === 'admin' ?*/}
+                                                {/*            <Link to='/admin/root/cash/transaction'>*/}
+                                                {/*                Payments*/}
+                                                {/*            </Link>*/}
+                                                {/*            :*/}
+                                                {/*            <Link to='/admin/cash/transaction'>*/}
+                                                {/*                Payments*/}
+                                                {/*            </Link>*/}
+                                                {/*    }*/}
 
-                                        </MenuItem>
+                                                {/*</MenuItem>*/}
+                                                <>
+                                                    {
+                                                        this.state.role === 'admin' ?
+                                                            <>
+                                                                <SubMenu title='Server' icon={<BsServer/>}>
+                                                                    <MenuItem>
+                                                                        <Link to='/admin/root/server'>
+                                                                            Servers
+                                                                        </Link>
+                                                                    </MenuItem>
+                                                                    <MenuItem>
+                                                                        <Link to='/admin/root/server-realms'>
+                                                                            Server-Realms
+                                                                        </Link>
+                                                                    </MenuItem>
+                                                                </SubMenu>
 
-                                        {/*<MenuItem icon={<MdAttachMoney/>}>*/}
-                                        {/*    {*/}
-                                        {/*        this.state.role === 'admin' ?*/}
-                                        {/*            <Link to='/admin/root/cash/transaction'>*/}
-                                        {/*                Payments*/}
-                                        {/*            </Link>*/}
-                                        {/*            :*/}
-                                        {/*            <Link to='/admin/cash/transaction'>*/}
-                                        {/*                Payments*/}
-                                        {/*            </Link>*/}
-                                        {/*    }*/}
+                                                                <SubMenu title='Tweak' icon={<AiOutlineCloudServer/>}>
+                                                                    <MenuItem>
+                                                                        <Link to='/admin/root/tweak'>
+                                                                            Tweaks
+                                                                        </Link>
+                                                                    </MenuItem>
+                                                                    <MenuItem>
+                                                                        <Link to='/admin/root/tweak-realms'>
+                                                                            Tweak-Realms
+                                                                        </Link>
+                                                                    </MenuItem>
+                                                                </SubMenu>
+                                                            </>
+                                                            : null
+                                                    }
+                                                </>
 
-                                        {/*</MenuItem>*/}
-                                        <>
-                                            {
-                                                this.state.role === 'admin' ?
-                                                    <>
-                                                        <SubMenu title='Server' icon={<BsServer/>}>
-                                                            <MenuItem>
-                                                                <Link to='/admin/root/server'>
-                                                                    Servers
-                                                                </Link>
-                                                            </MenuItem>
-                                                            <MenuItem>
-                                                                <Link to='/admin/root/server-realms'>
-                                                                    Server-Realms
-                                                                </Link>
-                                                            </MenuItem>
-                                                        </SubMenu>
-
-                                                        <SubMenu title='Tweak' icon={<AiOutlineCloudServer/>}>
-                                                            <MenuItem>
-                                                                <Link to='/admin/root/tweak'>
-                                                                    Tweaks
-                                                                </Link>
-                                                            </MenuItem>
-                                                            <MenuItem>
-                                                                <Link to='/admin/root/tweak-realms'>
-                                                                    Tweak-Realms
-                                                                </Link>
-                                                            </MenuItem>
-                                                        </SubMenu>
-                                                    </>
-                                                    : null
-                                            }
-                                        </>
-
-                                    </Menu>
-                                </ProSidebar>
-                            </li>
-                        </Nav>
-                    </div>
-                </div>
-                {/*}*/}
+                                            </Menu>
+                                        </ProSidebar>
+                                    </li>
+                                </Nav>
+                            </div>
+                        </div>
+                }
             </>
         );
     }
