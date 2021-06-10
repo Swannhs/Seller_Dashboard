fc868bb127a52b060119355ea25c6584e3115759
Swannhs <princeapurbask@gmail.com>
2021-06-10 14:35:50 +0600

    Resolve cookie and removed payment related info for mobile

--- src/views/Transaction/AgentTransaction/TransactionSentMobileApi.js
+++ src/views/Transaction/AgentTransaction/TransactionSentMobileApi.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import Cookies from "universal-cookie/lib";
 import RadiusApi from "../../../radius-api/RadiusApi";
 import TransactionSentMobile from "./TransactionSentMobile";
 import {Pagination} from "semantic-ui-react";
@@ -19,11 +18,9 @@ class TransactionSentMobileApi extends Component {
     }
 
     onApiCall = () => {
-        let cookie = new Cookies();
-
         RadiusApi.get('/voucher-transactions/view.json', {
             params: {
-                token: cookie.get('Token'),
+                token: localStorage.getItem('Token'),
                 key: this.props.id,
                 page: this.state.page,
                 start: this.state.start,
