20bb7a9ad778fe5ab85b037a1c280b2461186d27
Swannhs <princeapurbask@gmail.com>
2021-06-19 10:06:10 +0600

    Reconfigured for admin only

--- src/components/Dropdown/VoucherGroup.js
+++ src/components/Dropdown/VoucherGroup.js
@@ -17,12 +17,21 @@ class VoucherGroup extends Component {
             }
         })
             .then(response => {
-                this.setState({
-                    select: response.data.items.length == 1 ? response.data.items[0].id : '',
-                    group: response.data.items
-                })
+                if (localStorage.getItem('Role') === 'admin') {
+                    this.setState({
+                        select: '',
+                        group: response.data.items
+                    })
+                } else {
+                    this.setState({
+                        select: response.data.items.length === 1 ? response.data.items[0].id : '',
+                        group: response.data.items
+                    })
+                }
+
             })
     }
+
 
     componentDidUpdate(prevProps, prevState, snapshot) {
         if (prevState !== this.state) {
@@ -40,28 +49,28 @@ class VoucherGroup extends Component {
 
     render() {
         return (
-            this.state.group.length == 1 ? <></> :
-            <>
-                <h4 className='text-black-50'>Vendor</h4>
+            this.state.group.length === 1 && localStorage.getItem('Role') === 'admin' ? <></> :
+                <>
+                    <h4 className='text-black-50'>Vendor</h4>
 
-                <div className="form-group input-group">
-                    <div className="input-group-prepend">
-                        <span className="input-group-text"> <i className="fab fa-cloudscale"/> </span>
+                    <div className="form-group input-group">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text"> <i className="fab fa-cloudscale"/> </span>
+                        </div>
+                        <select className="form-control text"
+                                value={this.state.select}
+                                onChange={event => this.onHandleChange(event)}>
+                            <option>Choose...</option>
+                            {this.state.group.map((items) => {
+                                return (
+                                    <>
+                                        <option key={items.id} value={items.id}>{items.name}</option>
+                                    </>
+                                )
+                            })}
+                        </select>
                     </div>
-                    <select className="form-control text"
-                            value={this.state.select}
-                            onChange={event => this.onHandleChange(event)}>
-                        <option>Choose...</option>
-                        {this.state.group.map((items) => {
-                            return (
-                                <>
-                                    <option key={items.id} value={items.id}>{items.name}</option>
-                                </>
-                            )
-                        })}
-                    </select>
-                </div>
-            </>
+                </>
         );
     }
 }
