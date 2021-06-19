796a5b27e90400a19be602fc01a0fa2014f8f9bd
noman-bk <abu.noman@bkash.com>
2021-06-19 21:43:42 +0600

    Fix toast message for generate balance

--- src/views/Transaction/GenerateBalance.js
+++ src/views/Transaction/GenerateBalance.js
@@ -3,6 +3,7 @@ import RadiusApi from "../../radius-api/RadiusApi";
 import VoucherGroup from "../../components/Dropdown/VoucherGroup";
 import VoucherProfile from "../../components/Dropdown/VoucherProfile";
 import {Link} from "react-router-dom";
+import {toast, ToastContainer} from "react-toastify";
 
 class GenerateBalance extends Component {
     state = {
@@ -23,6 +24,7 @@ class GenerateBalance extends Component {
         })
             .then(response => {
                 if (response.data.success) {
+                    this.props.history.push('/admin/root/voucher/transaction')
                     toast.success(response.data.message, {
                         position: "top-right",
                         autoClose: 5000,
@@ -32,7 +34,6 @@ class GenerateBalance extends Component {
                         draggable: true,
                         progress: undefined,
                     });
-                    this.props.history.push('/admin/root/voucher/transaction')
                 } else {
                     toast.error(response.data.message, {
                         position: "top-right",
@@ -61,47 +62,50 @@ class GenerateBalance extends Component {
 
     render() {
         return (
-            <div className='container'>
-                <div className='ml-3'>
-                    <Link to='/admin/root/voucher/transaction'>
-                        <button className='ui button'>Back</button>
-                    </Link>
-                </div>
-                <article className="card-body mx-auto" style={{maxWidth: '350px', fontSize: '20px'}}>
-                    <VoucherGroup onChange={this.onCreateGroup}/>
-                    <VoucherProfile onChange={this.onCreateProfile}/>
-
-                    <h4 className='mt-3 text-black-50'>Amount</h4>
-
-                    <div className="form-group input-group">
-                        <div className="input-group-prepend">
-                            <span className="input-group-text"> <i className="fa fa-table"/> </span>
-                        </div>
-                        <input className="form-control" placeholder="Amount" type="text"
-                               value={this.state.transfer_amount}
-                               onChange={event => this.setState({transfer_amount: event.target.value})}
-                               required={true}
-                        />
+            <>
+                <ToastContainer/>
+                <div className='container'>
+                    <div className='ml-3'>
+                        <Link to='/admin/root/voucher/transaction'>
+                            <button className='ui button'>Back</button>
+                        </Link>
                     </div>
+                    <article className="card-body mx-auto" style={{maxWidth: '350px', fontSize: '20px'}}>
+                        <VoucherGroup onChange={this.onCreateGroup}/>
+                        <VoucherProfile onChange={this.onCreateProfile}/>
 
-                    {/*<h4 className='mt-3 text-black-50'>Quantity Rate</h4>*/}
+                        <h4 className='mt-3 text-black-50'>Amount</h4>
 
-                    {/*<div className="form-group input-group">*/}
-                    {/*    <div className="input-group-prepend">*/}
-                    {/*        <span className="input-group-text"> <i className="fa fa-table"/> </span>*/}
-                    {/*    </div>*/}
-                    {/*    <input className="form-control" placeholder="Rate" type="text"*/}
-                    {/*           value={this.state.quantity_rate}*/}
-                    {/*           onChange={event => this.setState({quantity_rate: event.target.value})}*/}
-                    {/*           required={true}*/}
-                    {/*    />*/}
-                    {/*</div>*/}
+                        <div className="form-group input-group">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text"> <i className="fa fa-table"/> </span>
+                            </div>
+                            <input className="form-control" placeholder="Amount" type="text"
+                                value={this.state.transfer_amount}
+                                onChange={event => this.setState({transfer_amount: event.target.value})}
+                                required={true}
+                            />
+                        </div>
 
-                    <button className='ui button positive mt-2' onClick={this.onGenerate}>
-                        Generate
-                    </button>
-                </article>
-            </div>
+                        {/*<h4 className='mt-3 text-black-50'>Quantity Rate</h4>*/}
+
+                        {/*<div className="form-group input-group">*/}
+                        {/*    <div className="input-group-prepend">*/}
+                        {/*        <span className="input-group-text"> <i className="fa fa-table"/> </span>*/}
+                        {/*    </div>*/}
+                        {/*    <input className="form-control" placeholder="Rate" type="text"*/}
+                        {/*           value={this.state.quantity_rate}*/}
+                        {/*           onChange={event => this.setState({quantity_rate: event.target.value})}*/}
+                        {/*           required={true}*/}
+                        {/*    />*/}
+                        {/*</div>*/}
+
+                        <button className='ui button positive mt-2' onClick={this.onGenerate}>
+                            Generate
+                        </button>
+                    </article>
+                </div>
+            </>
         );
     }
 }
